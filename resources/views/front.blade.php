b1033f5462fd9baea65b322ce939c9d286556be8
Fordyce Gozali <forddyce92@gmail.com>
2025-07-25 16:22:09 +0700

    chore: update author

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Fordyce Gozali <forddyce92@gmail.com>">
 
     {{-- Inline script to detect system dark mode preference and apply it immediately --}}
     <script>
