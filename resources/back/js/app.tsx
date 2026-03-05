af61e19323112e00e685c762db4c23e44f88c33f
Fordyce Gozali <forddyce92@gmail.com>
2026-03-05 22:03:52 +0700

    fix: app title

--- resources/back/js/app.tsx
+++ resources/back/js/app.tsx
@@ -6,7 +6,7 @@ import { SnackbarProvider } from 'notistack';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'WE Coffee Roasters';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
